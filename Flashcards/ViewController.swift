79e8c8ca1355761aaaf6b6f822812a9fdac08384
Emily Tomz <ertomz@gmail.com>
2020-02-21 20:55:44 -0800

    Simple Flashcards lab done

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Emily on 2/8/20.
+//  Copyright © 2020 Emily Tomz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+    }
+    
+}
+
