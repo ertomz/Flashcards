ac8888df82de31ef686fb34d126dcd8d179a1942
Emily Tomz <ertomz@gmail.com>
2020-02-21 22:03:58 -0800

    Optional features added!

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,14 +12,76 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        card.layer.cornerRadius = 20.0 // round edges
+        card.layer.shadowRadius = 15.0 // add shadows
+        card.layer.shadowOpacity = 0.2
+        
+        frontLabel.layer.cornerRadius = 20.0
+        frontLabel.layer.shadowRadius = 15.0
+        frontLabel.layer.shadowOpacity = 0.2
+        
+        backLabel.layer.cornerRadius = 20.0
+        backLabel.layer.shadowRadius = 15.0
+        backLabel.layer.shadowOpacity = 0.2
+    
+        // clips labels so the modifications show up
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        
+        btnOptionOne.layer.cornerRadius = 20.0
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        
+        btnOptionTwo.layer.cornerRadius = 20.0
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        
+        btnOptionThree.layer.cornerRadius = 20.0
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        
+        btnOptionOne.clipsToBounds = true
+        
         // Do any additional setup after loading the view.
     }
 
+    // If taps the question, nothing happens
+    // If taps the answer, goes back to the question
     @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if frontLabel.isHidden == true {
+            frontLabel.isHidden = false
+            btnOptionOne.isHidden = false
+            btnOptionTwo.isHidden = false
+            btnOptionThree.isHidden = false
+        }
+    }
+   
+    // If taps Button 1, hides the button
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    
+    // If taps Button 2, hides all the buttons and reveals the correct answer!
+    @IBAction func didTapOptionTwo(_ sender: Any) {
         frontLabel.isHidden = true
+        btnOptionOne.isHidden = true
+        btnOptionTwo.isHidden = true
+        btnOptionThree.isHidden = true
+    }
+    
+    // If taps Button 3, hides the button
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
     }
     
 }
